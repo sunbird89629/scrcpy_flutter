c1dcd9a4f7dd4d3344f78f10e75cb6384ad9c71d
Howard Wang <sunbird.work@gmail.com>
2026-04-24 21:14:53 +0800

    feat: add fvp support and implement raw H.264 streaming harness

--- apps/desktop/macos/Flutter/GeneratedPluginRegistrant.swift
+++ apps/desktop/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,14 +5,18 @@
 import FlutterMacOS
 import Foundation
 
+import fvp
 import media_kit_libs_macos_video
 import media_kit_video
 import package_info_plus
+import video_player_avfoundation
 import wakelock_plus
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FvpPlugin.register(with: registry.registrar(forPlugin: "FvpPlugin"))
   MediaKitLibsMacosVideoPlugin.register(with: registry.registrar(forPlugin: "MediaKitLibsMacosVideoPlugin"))
   MediaKitVideoPlugin.register(with: registry.registrar(forPlugin: "MediaKitVideoPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
+  FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
   WakelockPlusMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockPlusMacosPlugin"))
 }
