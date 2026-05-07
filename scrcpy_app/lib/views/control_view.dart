1f894ce42e14e3817d5fca447a015badab468f04
Howard Wang <sunbird.work@gmail.com>
2026-05-07 11:48:04 +0800

    style: apply shaded layer background to ControlView

--- scrcpy_app/lib/views/control_view.dart
+++ scrcpy_app/lib/views/control_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:scrcpy_app/app_controller.dart';
 import 'package:scrcpy_app/mcp_server_panel.dart';
+import 'package:scrcpy_app/theme/app_theme.dart';
 import 'package:scrcpy_app/views/control_button_widget.dart';
 
 class ControlView extends StatelessWidget {
@@ -11,21 +12,27 @@ class ControlView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final AppController appController = AppController();
+    final sidebarColor =
+        Theme.of(context).extension<AppColors>()?.sidebarBackground;
+
     return Expanded(
-      child: ListenableBuilder(
-        listenable: appController.mcpServerController,
-        builder: (_, __) {
-          return Column(
-            children: [
-              Expanded(
-                child: McpServerPanel(
-                  controller: appController.mcpServerController,
+      child: Container(
+        color: sidebarColor,
+        child: ListenableBuilder(
+          listenable: appController.mcpServerController,
+          builder: (_, __) {
+            return Column(
+              children: [
+                Expanded(
+                  child: McpServerPanel(
+                    controller: appController.mcpServerController,
+                  ),
                 ),
-              ),
-              ControlButtonWidget(),
-            ],
-          );
-        },
+                ControlButtonWidget(),
+              ],
+            );
+          },
+        ),
       ),
     );
   }
